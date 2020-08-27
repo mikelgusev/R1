01c3bc5fb3d39de6170966f9996639c44d5464b9
mikel <1024m@mail.ru>
2020-08-27 10:31:17 +0300

    New Alg

--- R1/Program.cs
+++ R1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace R1
 {
@@ -8,39 +9,37 @@ namespace R1
     {
         static void Main(string[] args)
         {
-            4.Perm().DistinctAndSort().ToStr().Show();
-            "aabc".Perm().DistinctAndSort().ToStr().Show();
-            "dd".Perm().DistinctAndSort().ToStr().Show();
+            "abbc".Do();
+            new[] { "a", "bb", "b", "c" }.Do();
+            "ab".Do();
+            "a".Do();
+            "".Do();
+            Console.ReadLine();
         }
     }
-    public static class E
+    public static class EAlg
     {
-        public static T It<T>(this T x) => x;
-        public static void Show(this string s) => Console.WriteLine(s);
-        public static IEnumerable<IEnumerable<int>> Perm(this int n) => Enumerable.Range(1, n).Perm();
-        public static string ToStr<T>(this IEnumerable<IEnumerable<T>> x) => "[" + string.Join('\n', x.Select(y => string.Join(' ', y))) + "]";
-        public static IEnumerable<IEnumerable<T>> DistinctAndSort<T>(this IEnumerable<IEnumerable<T>> x) => x.Distinct(new Eq<T>()).OrderByDescending(It, new Cmp<T>()).Select(It);
-        public static IEnumerable<IEnumerable<T>> Perm<T>(this IEnumerable<T> ts) => ts.Count() < 2 ? new[] { ts } : ts.SelectMany((x, i) => ts.Where((y, j) => j != i).Perm().Select(t => t.Prepend(x)));
-
-        private class Eq<T> : EqualityComparer<IEnumerable<T>>
+        public static IEnumerable<IEnumerable<T>> Perm<T>(this IEnumerable<T> ts)
         {
-            public override int GetHashCode(IEnumerable<T> obj) => 0;
-            public override bool Equals(IEnumerable<T> x, IEnumerable<T> y) => x.SequenceEqual(y);
-        }
-
-        private class Cmp<T> : Comparer<IEnumerable<T>>
-        {
-            public override int Compare(IEnumerable<T> x, IEnumerable<T> y)
+            if (ts.Any())
             {
-                int rb;
-                bool xb, yb;
-                using var xe = x.GetEnumerator();
-                using var ye = y.GetEnumerator();
-                while ((xb = xe.MoveNext())&(yb = ye.MoveNext())) if ((rb = Comparer<T>.Default.Compare(xe.Current, ye.Current)) != 0) return rb;
-                return xb.CompareTo(yb);
+                var was = new HashSet<T>();
+                foreach (var (t,i) in ts.Select((t,i)=>(t,i)))
+                    if (was.Add(t))
+                        foreach (var s in ts.Take(i).Concat(ts.Skip(i + 1)).Perm())
+                            yield return s.Prepend(t);
             }
+            else yield return ts;
         }
     }
 
+    public static class ECommon
+    {
+        public static void Do<T>(this IEnumerable<T> ts) => ts.MyOrder().Perm().ToStr().Show();
+        public static void Show(this string s) => Console.WriteLine(s);
+        public static string ToStr<T>(this IEnumerable<IEnumerable<T>> x) => "(" + string.Join('\n', x.Select(y => string.Join(' ', y))) + ")";
+        public static T It<T>(T x) => x;
+        public static IEnumerable<T> MyOrder<T>(this IEnumerable<T> ts) => ts.OrderByDescending(It).Select(It);
+    }
 }
 
